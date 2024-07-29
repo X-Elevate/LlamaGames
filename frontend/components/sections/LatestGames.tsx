e2be9787c5ce09b80d2aa809b2dbc5c71e1a3cef
Stefan Todorov <79049907+StefanTodorov32@users.noreply.github.com>
2024-07-29 15:31:51 +0300

    Refactor CSS classes and update LatestGames section

--- frontend/components/sections/LatestGames.tsx
+++ frontend/components/sections/LatestGames.tsx
@@ -21,17 +21,17 @@ const games = [
 
 function LatestGames() {
   return (
-    <div className="w-full h-[900px] bg-gradient-to-tr from-gradientFirstColor to-gradientSecondColor mt-10 flex flex-col items-center font-dmMono">
+    <div className="w-full bg-gradient-to-tr from-gradientFirstColor to-gradientSecondColor pt-10 flex flex-col items-center font-dmMono pb-8 rounded-b-[80px]">
       <div className="flex flex-col gap-[1.5rem] w-[83%] mt-[2%]">
-        <h1 className="text-[2rem] text-orange">Latest games</h1>
+        <h1 className="text-[2rem] text-orange font-bold">Latest games</h1>
         <p className="text-[1.5rem] text-white">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore
         </p>
       </div>
-      <div className="flex w-[83%] mt-3 gap-12">
+      <div className="flex w-[83%] gap-20 justify-between pt-[2rem]">
         {games.map((game, index) => (
-            <div className="relative w-1/3 aspect-square overflow-hidden shadow-md rounded-[40px] bg-gradient-to-tr from-gradientFourthColor to-gradientThirdColor p-3" key={index}>
+            <div className="relative w-1/3 aspect-square overflow-hidden shadow-md rounded-[40px] bg-gradient-to-tr from-gradientFourthColor to-gradientThirdColor p-[1rem]" key={index}>
               <Image
                 src={game.image}
                 alt={game.name}
