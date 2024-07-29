c0af0a0e6a257365cf9a6901c3ef59a00e122b69
Kamen Kanev <kamenkanev88@gmail.com>
2024-07-29 16:19:52 +0300

    Update header and LatestGames styles

--- frontend/components/sections/LatestGames.tsx
+++ frontend/components/sections/LatestGames.tsx
@@ -21,8 +21,8 @@ const games = [
 
 function LatestGames() {
   return (
-    <div className="relative w-full bg-gradient-to-tr from-gradientFirstColor to-gradientSecondColor mt-10 flex flex-col items-center font-dmMono pb-10">
-      <div className="absolute inset-0 z-0">
+    <div className="relative w-full bg-gradient-to-tr from-gradientFirstColor to-gradientSecondColor mt-10 flex flex-col items-center font-dmMono pb-10 z-20 rounded-b-[80px]">
+      <div className="absolute inset-0 z-20">
         <div className='before:content-[""] before:absolute before:bg-[#FFBF40] before:opacity-[0.10] before:rounded-full before:blur-lg before:w-[150px] before:h-[150px] before:top-[-20px] before:left-[-30px] sm:before:w-[200px] sm:before:h-[200px] sm:before:top-[-30px] sm:before:left-[-45px] md:before:w-[250px] md:before:h-[250px] md:before:top-[-40px] md:before:left-[-55px] lg:before:w-[300px] lg:before:h-[300px] lg:before:top-[-50px] lg:before:left-[-60px]'></div>
       </div>
       <div className="relative flex flex-col gap-[1.5rem] w-[83%] mt-[2%] z-10">
