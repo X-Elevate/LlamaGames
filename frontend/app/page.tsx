71c37d2cbbf2dce53b3180b1f11b830027028790
Mityo Draganov <mityodraganow@gmail.com>
2024-07-29 14:17:55 +0300

    populate correct text

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <main className='flex min-h-screen flex-col items-center justify-between'>
             <div className='flex flex-col items-center gap-[10%] py-10'>
-                <HeroVideo />
+              
                 <WhoWeAre />
             </div>
         </main>
--- frontend/sections/WhoWeAre.tsx
+++ frontend/sections/WhoWeAre.tsx
@@ -1,19 +1,24 @@
-import { useRef, useEffect } from 'react';
-
 export default function WhoWeAre() {
-    const videoRef = useRef<HTMLVideoElement>(null);
+  return (
+    <section className="w-full flex justify-between items-center ">
+    
+        <div className="w-2/5"> 
+          <img
+            src="/WhoWeAre_lamas.svg"
+            alt="lamas banner"
+            className="w-full h-full object-cover"
+          />
+        </div>
 
-
-    return (
-        <section className='relative w-full overflow-hidden'>
-            <div className='flex w-full'>
-                <img src="/WhoWeAre_lamas.svg" alt="lamas baner" />
-
-                <div>
-                    <h2>Who we are</h2>
-                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.</p>
-                </div>
-            </div>
-        </section>
-    );
+        <div className="w-2/5">
+          <h2 className="text-2xl text-cherry">Who we are</h2>
+          <p className="text-sm text-darkCherry w-full"> 
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+            fugit, sed quia consequuntur magni dolores eos qui ratione
+            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.
+          </p>
+        </div>
+ 
+    </section>
+  );
 }
