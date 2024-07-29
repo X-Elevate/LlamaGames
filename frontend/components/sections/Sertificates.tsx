93ea1ca45e74b26ef8011baae93b5a33a7a2159e
Kamen Kanev <kamenkanev88@gmail.com>
2024-07-29 15:53:43 +0300

    Final touches on sertificates

--- frontend/components/sections/Sertificates.tsx
+++ frontend/components/sections/Sertificates.tsx
@@ -3,17 +3,17 @@
 export default function Sertificates() {
   return (
     <section className="w-[80%] flex flex-col gap-4 items-center justify-between">
-      <h2 className="text-cherry text-[32px] text-left w-full pl-6">
+      <h2 className="text-cherry text-[32px] text-left w-full pl-6 font-bold pb-10">
         You’re in good company
       </h2>
 
       <ul className="flex w-full justify-evenly">
         <li className="w-[30%] flex flex-col gap-4">
-          <div className="w-[30%]">
+          <div className="w-[30%] pb-8">
             <img src="sertificateImage.svg" className="w-full h-full object-cover"/>
           </div>
           <div className="flex flex-col gap-8">
-            <h3 className="text-[24px] text-orange">ISO/IEC 27001:2022</h3>
+            <h3 className="text-[24px] font-bold text-orange">ISO/IEC 27001:2022</h3>
             <p className="flex flex-col gap-4">
               <span className="text-darkCherry">
                 Llama Game Technology is accredited with the ISO/IEC 27001:2013,
@@ -21,16 +21,16 @@ export default function Sertificates() {
                 maintaining, and continually improving an information security
                 management system within the context of the organization.
               </span>
-              <span className="text-cherry font-medium hover:underline">Learn more</span>
+              <span className="text-cherry font-semibold hover:underline">Learn more</span>
             </p>
           </div>
         </li>
         <li className="w-[30%] flex flex-col gap-4">
-          <div className="w-[30%]">
+          <div className="w-[30%] pb-8">
             <img src="sertificateImage.svg" className="w-full h-full object-cover"/>
           </div>
           <div className="flex flex-col gap-8">
-            <h3 className="text-[24px] text-orange">ISO/IEC 27001:2022</h3>
+            <h3 className="text-[24px] font-bold text-orange">ISO/IEC 27001:2022</h3>
             <p className="flex flex-col gap-4">
               <span className="text-darkCherry">
                 Llama Game Technology is accredited with the ISO/IEC 27001:2013,
@@ -38,16 +38,16 @@ export default function Sertificates() {
                 maintaining, and continually improving an information security
                 management system within the context of the organization.
               </span>
-              <span className="text-cherry font-medium hover:underline">Learn more</span>
+              <span className="text-cherry font-semibold hover:underline">Learn more</span>
             </p>
           </div>
         </li>
         <li className="w-[30%] flex flex-col gap-4">
-          <div className="w-[30%]">
+          <div className="w-[30%] pb-8">
             <img src="sertificateImage.svg" className="w-full h-full object-cover"/>
           </div>
           <div className="flex flex-col gap-8">
-            <h3 className="text-[24px] text-orange">ISO/IEC 27001:2022</h3>
+            <h3 className="text-[24px] font-bold text-orange">ISO/IEC 27001:2022</h3>
             <p className="flex flex-col gap-4">
               <span className="text-darkCherry">
                 Llama Game Technology is accredited with the ISO/IEC 27001:2013,
@@ -55,7 +55,7 @@ export default function Sertificates() {
                 maintaining, and continually improving an information security
                 management system within the context of the organization.
               </span>
-              <span className="text-cherry font-medium hover:underline">Learn more</span>
+              <span className="text-cherry font-semibold hover:underline">Learn more</span>
             </p>
           </div>
         </li>
