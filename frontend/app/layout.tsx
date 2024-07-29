9e2c28206a94ce8b97eae56e88c08a69927c16ca
StefanTodorov32 <bgtrak@gmail.com>
2024-07-29 19:17:21 +0300

    Improve mobile responsiveness

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,41 +1,42 @@
-import type { Metadata } from 'next';
-import { DM_Mono as FontMono } from 'next/font/google';
-import './globals.css';
-import Header from '@/components/header';
-import { cn } from '@/lib/utils';
-import Footer from '@/components/footer';
-
+import type { Metadata } from "next";
+import { DM_Mono as FontMono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { cn } from "@/lib/utils";
+import Footer from "@/components/footer";
+import Head from "next/head";
 
 const fontMono = FontMono({
-    subsets: ['latin'],
-    weight: ['400', '500'],
-    variable: '--font-mono',
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  variable: "--font-mono",
 });
 
 export const metadata: Metadata = {
-    title: 'Llama Games',
-    description: 'Llama Games is a game development studio.',
+  title: "Llama Games",
+  description: "Llama Games is a game development studio.",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang='en'>
-            <body
-                className={cn(
-                    'min-h-screen bg-background font-mono antialiased',
-                    fontMono.variable
-                )}
-            >
-                <Header />
-                <main className='flex min-h-screen flex-col'>
-                    {children}
-                </main>
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-mono antialiased",
+          fontMono.variable
+        )}
+      >
+        <Header />
+        <main className="flex min-h-screen flex-col">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
