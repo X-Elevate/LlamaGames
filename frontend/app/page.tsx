acc7659abccaa54605141df076c69e287a347fbe
Mityo Draganov <mityodraganow@gmail.com>
2024-07-29 14:09:06 +0300

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,8 +4,8 @@ import WhoWeAre from '@/sections/WhoWeAre';
 
 export default function Home() {
     return (
-        <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            <div className='flex flex-col items-center'>
+        <main className='flex min-h-screen flex-col items-center justify-between'>
+            <div className='flex flex-col items-center gap-[10%] py-10'>
                 <HeroVideo />
                 <WhoWeAre />
             </div>
