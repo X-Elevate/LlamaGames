92c49925284c15f2624360e9fc3b162003de2c4c
Stefan Todorov <79049907+StefanTodorov32@users.noreply.github.com>
2024-07-29 15:49:47 +0300

    Add our partner section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,6 +5,7 @@ import ProudBulgarians from '../components/sections/ProudBulgarians';
 import HeroVideo from '@/components/sections/HeroVideo';
 import Sertificates from '@/components/sections/Sertificates';
 import LatestGames from '@/components/sections/LatestGames';
+import OurPartners from '@/components/sections/OurPartners';
 
 export default function Home() {
     return (
@@ -15,6 +16,7 @@ export default function Home() {
                 <ProudBulgarians />
                 <LatestGames />
                 <Sertificates />
+                <OurPartners />
             </div>
         </main>
     );
