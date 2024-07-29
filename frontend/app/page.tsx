7391b271545158fef499873dae4fd27d15b85fe4
Kamen Kanev <kamenkanev88@gmail.com>
2024-07-29 14:36:42 +0300

    changed directories

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,12 @@
-"use client"
-import HeroVideo from '@/app/sections/HeroVideo';
+'use client';
 
-import WhoWeAre from '@/app/sections/WhoWeAre';
-import ProudBulgarians from './sections/ProudBulgarians';
+import WhoWeAre from '@/components/sections/WhoWeAre';
+import ProudBulgarians from '../components/sections/ProudBulgarians';
 
 export default function Home() {
     return (
         <main className='flex min-h-screen flex-col items-center justify-between'>
             <div className='flex flex-col items-center gap-[10%] py-10'>
-              
                 <WhoWeAre />
                 <ProudBulgarians />
             </div>
