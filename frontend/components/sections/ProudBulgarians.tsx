a43d4064ecfce72113bfb050d9a326a9886779e3
Mityo Draganov <mityodraganow@gmail.com>
2024-07-29 14:41:58 +0300

    proud bulgarians section done

--- frontend/components/sections/ProudBulgarians.tsx
+++ frontend/components/sections/ProudBulgarians.tsx
@@ -2,21 +2,21 @@
 
 export default function ProudBulgarians() {
   return (
-    <section className="w-3/4 flex">
-      <div className="w-5/12">
-        <h2 className="text-2xl text-cherry">Proud Bulgarians</h2>
-        <p className="text-sm text-darkCherry w-full">
+    <section className="w-3/4 flex items-center justify-between">
+      <div className="w-1/2">
+        <h2 className="text-[32px] text-cherry">Proud Bulgarians</h2>
+        <p className="text-[16px] text-darkCherry w-full">
           Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
           fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem
           sequi nesciunt. Neque porro quisquam est, qui dolorem.
         </p>
       </div>
 
-      <div className="w-2/5 h-full">
+      <div className="w-[30%] h-full">
         <img
           src="ProudBulgariansGlobe.svg"
           alt="lamas banner"
-          className="w-full h-full object-cover"
+          className="w-full aspect-square object-cover"
         />
       </div>
     </section>
