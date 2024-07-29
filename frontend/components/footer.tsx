5bcf8945e40ad8ea1a8a6fd304ecbbbb34962acf
Kamen Kanev <kamenkanev88@gmail.com>
2024-07-29 13:14:31 +0300

    Update footer.tsx

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -5,15 +5,15 @@ export default function Footer() {
     const textColor = '#FFF4D6';
     return (
         <footer
-            className='bg-gradient-to-tr from-gradientFirstColor to-gradientSecondColor py-8 px-4 h-[5%]'
+            className='bg-gradient-to-tr from-gradientFirstColor to-gradientSecondColor py-8 pt-20 px-4 h-[5%]'
             style={{
                 borderTopLeftRadius: '5rem',
                 borderTopRightRadius: '5rem',
                 color: textColor,
             }}
         >
-            <div className='container ml-32 flex flex-col md:flex-row justify-between items-center'>
-                <div className='grid grid-cols-2 gap-x-16 gap-y-2 mb-6 md:mb-0'>
+            <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
+                <div className='grid grid-cols-2 gap-x-32 gap-y-4 mb-6 md:mb-0'>
                     <Link href='/about' className='hover:underline '>
                         About us
                     </Link>
@@ -34,7 +34,7 @@ export default function Footer() {
                     </Link>
                 </div>
 
-                <div className='mb-6 md:mb-0'>
+                <div className='mb-6 md:mb-0 flex justify-center items-center'>
                     <Image
                         src='/footer-logo.svg'
                         alt='LlamaGames Logo'
@@ -43,7 +43,7 @@ export default function Footer() {
                     />
                 </div>
 
-                <div className='flex space-x-4'>
+                <div className='flex space-x-7 justify-center items-center'>
                     <SocialIcon href='#' icon='linkedin' />
                     <SocialIcon href='#' icon='facebook' />
                     <SocialIcon href='#' icon='twitter' />
@@ -51,9 +51,12 @@ export default function Footer() {
                 </div>
             </div>
 
-            <div className='border-t border-purple-800 mt-8 pt-4 text-center text-xs'>
+            <div
+                className='border-t mt-8 pt-4 text-center text-xs flex justify-center items-center '
+                style={{ borderColor: '#D1D4D7', gap: '24px' }}
+            >
                 <p>© Copyright 2024 LLama Games / All rights reserved.</p>
-                <Link href='/terms' className='hover:underline mr-4'>
+                <Link href='/terms' className='hover:underline'>
                     Terms
                 </Link>
                 <Link href='/privacy' className='hover:underline'>
@@ -66,7 +69,7 @@ export default function Footer() {
 
 function SocialIcon({ href, icon }: { href: string; icon: string }) {
     return (
-        <Link href={href}>
+        <Link href={href} className='flex justify-center items-center'>
             <Image
                 src={`/${icon}-icon.svg`}
                 alt={`${icon} icon`}
