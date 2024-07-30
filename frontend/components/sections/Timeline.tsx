eaa30127a625be08ea54d43092920e38611af407
Kamen Kanev <kamenkanev88@gmail.com>
2024-07-30 16:50:44 +0300

    Update Timeline.tsx

--- frontend/components/sections/Timeline.tsx
+++ frontend/components/sections/Timeline.tsx
@@ -16,12 +16,6 @@ const TimelineEvent = ({
 }) => (
   <div className="flex flex-col items-center text-cherry">
     <div className="flex flex-col items-center">
-      {/* {position === "top" && (
-        <div className="mb-2 text-center max-w-xs text-sm">{text}</div>
-      )} */}
-
-      {/* {position === "bottom" && <div className="font-bold mb-2">{year}</div>} */}
-
       <div className="w-4 h-4 bg-cherry rounded-full relative z-10">
         <div className="w-7 h-7 border-4 border-cherry rounded-full absolute top-[1/2] left-1/2 transform -translate-x-1/2 -translate-y-[20%]"></div>
         <div
@@ -63,8 +57,8 @@ const Timeline = () => {
   ];
 
   return (
-    <section className="bg-[#F0CDB7] p-8 rounded-b-[80px] w-full h-[32rem] flex items-center justify-center">
-      <div className="relative py-8 w-full px-24">
+    <section className="bg-[#F0CDB7] p-36 rounded-b-[80px] w-full h-[32rem] flex items-center justify-center">
+      <div className="relative py-8 w-full px-36">
         <div className="absolute top-1/2 left-0 right-0 h-0.5 bg-darkCherry transform -translate-y-1/2"></div>
         <div className="flex justify-between relative z-10">
           {events.map((event, index) => (
